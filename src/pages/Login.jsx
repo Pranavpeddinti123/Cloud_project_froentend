5f2972598e97639777e9185a12231356c90c8d00
Pranavpeddinti123 <163733336+Pranavpeddinti123@users.noreply.github.com>
2025-09-23 22:36:12 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -28,8 +28,8 @@ const Login = () => {
     try {
       const url =
         currentState === "Login"
-          ? "http://ec2-13-218-220-63.compute-1.amazonaws.com:8081/api/auth/login"
-          : "http://ec2-13-218-220-63.compute-1.amazonaws.com:8081/api/auth/register";
+          ? "http://ec2-54-83-122-162.compute-1.amazonaws.com:8081/api/auth/login"
+          : "http://ec2-54-83-122-162.compute-1.amazonaws.com:8081/api/auth/register";
 
       const bodyData =
         currentState === "Login"
